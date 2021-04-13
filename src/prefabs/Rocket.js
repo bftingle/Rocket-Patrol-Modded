68f7c438c013c94e74bd4404bc62e7d1bbaf8ecb
Ben <bftingle@ucsc.edu>
2021-04-13 02:10:45 -0700

    Dodge!

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -4,7 +4,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         scene.add.existing(this);
         this.isFiring = false;
-        this.moveSpeed = 2;
+        this.moveSpeed = 1;
 
         this.sfxRocket = scene.sound.add('sfx_rocket');
     }
@@ -28,7 +28,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.reset();
         }
         if(Phaser.Input.Keyboard.JustDown(keySPACE) && !this.isFiring) {
+            this.moveSpeed = 3;
             this.play('fighterflip');
+            this.on('animationcomplete', () => {
+                this.moveSpeed = 1;
+            });
             this.sfxRocket.play();
         }
     }
